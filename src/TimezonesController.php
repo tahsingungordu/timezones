6069cdbb1281922dcaf8116f33bea2935f8124a0
Tahsin Gungordu <tahsingungordu@gmail.com>
2017-09-26 21:24:25 +0300

    Creating ServiceProvider, Controller and composer file

---
+++ src/TimezonesController.php
@@ -0,0 +1,10 @@
+<?php
+namespace Bymega\Timezones;
+
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+
+class TimezonesController extends Controller
+{
+    //
+}
---
+++ src/TimezonesServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Bymega\Timezones;
+
+use Illuminate\Support\ServiceProvider;
+
+class TimezonesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
